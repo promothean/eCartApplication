ca63c266b17bd46aacaf962f2e2326bdcf917d01
Inzmam <ishuinzu@gmail.com>
2021-03-15 19:14:40 +0500

    Added Comments Removed UI Code

--- app/src/main/java/com/promothean/ecartapplication/NotificationsActivity.java
+++ app/src/main/java/com/promothean/ecartapplication/NotificationsActivity.java
@@ -2,25 +2,13 @@ package com.promothean.ecartapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.LinearLayout;
-
-import com.promothean.ecartapplication.adapter.NotificationAdapter;
-import com.promothean.ecartapplication.model.Notification;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class NotificationsActivity extends AppCompatActivity {
     private static final String TAG = "NotificationsActivity";
     private Toolbar toolBar;
-    private LinearLayout layoutNoNotifications;
-    private RecyclerView recyclerNotifications;
-    private NotificationAdapter notificationAdapter;
-    private List<Notification> notifications;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +16,6 @@ public class NotificationsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_notifications);
 
         toolBar = findViewById(R.id.toolBar);
-        layoutNoNotifications = findViewById(R.id.layoutNoNotifications);
-        recyclerNotifications = findViewById(R.id.recyclerNotifications);
-
-        recyclerNotifications.setVisibility(View.GONE);
-        layoutNoNotifications.setVisibility(View.VISIBLE);
 
         setSupportActionBar(toolBar);
         toolBar.setNavigationOnClickListener(new View.OnClickListener() {
@@ -41,27 +24,5 @@ public class NotificationsActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
-
-        loadNotifications();
-    }
-
-    private void loadNotifications() {
-        notifications = new ArrayList<>();
-
-        notifications.add(new Notification("New Notifcation", "Notification Content", "13/03/2021"));
-        notifications.add(new Notification("New Notifcation", "Notification Content", "13/03/2021"));
-        notifications.add(new Notification("New Notifcation", "Notification Content", "13/03/2021"));
-        notifications.add(new Notification("New Notifcation", "Notification Content", "13/03/2021"));
-        notifications.add(new Notification("New Notifcation", "Notification Content", "13/03/2021"));
-        notifications.add(new Notification("New Notifcation", "Notification Content", "13/03/2021"));
-        notifications.add(new Notification("New Notifcation", "Notification Content", "13/03/2021"));
-        notifications.add(new Notification("New Notifcation", "Notification Content", "13/03/2021"));
-        notifications.add(new Notification("New Notifcation", "Notification Content", "13/03/2021"));
-        notifications.add(new Notification("New Notifcation", "Notification Content", "13/03/2021"));
-
-        notificationAdapter = new NotificationAdapter(NotificationsActivity.this, notifications);
-        recyclerNotifications.setAdapter(notificationAdapter);
-        layoutNoNotifications.setVisibility(View.GONE);
-        recyclerNotifications.setVisibility(View.VISIBLE);
     }
 }
