f99c16ec2998072880d804af97a856321b54be90
Inzmam <ishuinzu@gmail.com>
2021-03-15 19:24:38 +0500

    Added Comments Removed UI Code

--- app/src/main/java/com/promothean/ecartapplication/navfragments/FavoriteFragment.java
+++ app/src/main/java/com/promothean/ecartapplication/navfragments/FavoriteFragment.java
@@ -3,26 +3,15 @@ package com.promothean.ecartapplication.navfragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 
 import com.promothean.ecartapplication.R;
-import com.promothean.ecartapplication.adapter.FavoriteProductAdapter;
-import com.promothean.ecartapplication.model.FavoriteProduct;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class FavoriteFragment extends Fragment {
     private static final String TAG = "FavoriteFragment";
-    private LinearLayout layoutNoFavoriteProduct;
-    private RecyclerView recyclerFavoriteProducts;
-    private FavoriteProductAdapter favoriteProductAdapter;
-    private List<FavoriteProduct> favoriteProducts;
 
     public FavoriteFragment() { }
 
@@ -30,44 +19,6 @@ public class FavoriteFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_favorite, container, false);
 
-        layoutNoFavoriteProduct = view.findViewById(R.id.layoutNoFavoriteProduct);
-        recyclerFavoriteProducts = view.findViewById(R.id.recyclerFavoriteProducts);
-
-        //Back End Coding
-        layoutNoFavoriteProduct.setVisibility(View.VISIBLE);
-        recyclerFavoriteProducts.setVisibility(View.GONE);
-        loadFavoriteProducts();
-
         return view;
-    }
-
-    private void loadFavoriteProducts() {
-        favoriteProducts = new ArrayList<>();
-
-        favoriteProducts.add(new FavoriteProduct(R.drawable.img_hot_deals_1, "Favorite Product ", "$130", "Added On : 13/03/2021"));
-        favoriteProducts.add(new FavoriteProduct(R.drawable.img_hot_deals_2, "Favorite Product ", "$130", "Added On : 13/03/2021"));
-        favoriteProducts.add(new FavoriteProduct(R.drawable.img_hot_deals_3, "Favorite Product ", "$130", "Added On : 13/03/2021"));
-        favoriteProducts.add(new FavoriteProduct(R.drawable.img_hot_deals_4, "Favorite Product ", "$130", "Added On : 13/03/2021"));
-        favoriteProducts.add(new FavoriteProduct(R.drawable.img_hot_deals_1, "Favorite Product ", "$130", "Added On : 13/03/2021"));
-        favoriteProducts.add(new FavoriteProduct(R.drawable.img_hot_deals_2, "Favorite Product ", "$130", "Added On : 13/03/2021"));
-        favoriteProducts.add(new FavoriteProduct(R.drawable.img_hot_deals_3, "Favorite Product ", "$130", "Added On : 13/03/2021"));
-        favoriteProducts.add(new FavoriteProduct(R.drawable.img_hot_deals_4, "Favorite Product ", "$130", "Added On : 13/03/2021"));
-        favoriteProducts.add(new FavoriteProduct(R.drawable.img_hot_deals_1, "Favorite Product ", "$130", "Added On : 13/03/2021"));
-        favoriteProducts.add(new FavoriteProduct(R.drawable.img_hot_deals_2, "Favorite Product ", "$130", "Added On : 13/03/2021"));
-        favoriteProducts.add(new FavoriteProduct(R.drawable.img_hot_deals_3, "Favorite Product ", "$130", "Added On : 13/03/2021"));
-        favoriteProducts.add(new FavoriteProduct(R.drawable.img_hot_deals_4, "Favorite Product ", "$130", "Added On : 13/03/2021"));
-        favoriteProducts.add(new FavoriteProduct(R.drawable.img_hot_deals_1, "Favorite Product ", "$130", "Added On : 13/03/2021"));
-        favoriteProducts.add(new FavoriteProduct(R.drawable.img_hot_deals_2, "Favorite Product ", "$130", "Added On : 13/03/2021"));
-        favoriteProducts.add(new FavoriteProduct(R.drawable.img_hot_deals_3, "Favorite Product ", "$130", "Added On : 13/03/2021"));
-        favoriteProducts.add(new FavoriteProduct(R.drawable.img_hot_deals_4, "Favorite Product ", "$130", "Added On : 13/03/2021"));
-        favoriteProducts.add(new FavoriteProduct(R.drawable.img_hot_deals_1, "Favorite Product ", "$130", "Added On : 13/03/2021"));
-        favoriteProducts.add(new FavoriteProduct(R.drawable.img_hot_deals_2, "Favorite Product ", "$130", "Added On : 13/03/2021"));
-        favoriteProducts.add(new FavoriteProduct(R.drawable.img_hot_deals_3, "Favorite Product ", "$130", "Added On : 13/03/2021"));
-        favoriteProducts.add(new FavoriteProduct(R.drawable.img_hot_deals_4, "Favorite Product ", "$130", "Added On : 13/03/2021"));
-
-        favoriteProductAdapter = new FavoriteProductAdapter(this.getContext(), favoriteProducts);
-        recyclerFavoriteProducts.setAdapter(favoriteProductAdapter);
-        layoutNoFavoriteProduct.setVisibility(View.GONE);
-        recyclerFavoriteProducts.setVisibility(View.VISIBLE);
     }
 }
