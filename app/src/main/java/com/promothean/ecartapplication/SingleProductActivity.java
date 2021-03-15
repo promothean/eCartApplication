0c9e0e901521693f618a399d12d2ac2176c1fccb
Inzmam <ishuinzu@gmail.com>
2021-03-15 21:27:58 +0500

    Added Comments Removed UI Code

--- app/src/main/java/com/promothean/ecartapplication/SingleProductActivity.java
+++ app/src/main/java/com/promothean/ecartapplication/SingleProductActivity.java
@@ -1,15 +1,29 @@
 package com.promothean.ecartapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class SingleProductActivity extends AppCompatActivity {
     private static final String TAG = "SingleProductActivity";
+    private Toolbar toolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_product);
+
+        toolBar = findViewById(R.id.toolBar);
+
+        setSupportActionBar(toolBar);
+
+        toolBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
